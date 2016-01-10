d0acfc36eb053cb90f54c5e37e200b1d9f4bb330
Adam Armour <adam.armour@gmail.com>
2016-01-10 14:47:00 -0700

    feat(routes): handle 404 and other errors

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var keystone = require('keystone');
 var middleware = require('./middleware');
 var importRoutes = keystone.importer(__dirname);
@@ -5,6 +7,24 @@ var importRoutes = keystone.importer(__dirname);
 // Common Middleware
 keystone.pre('routes', middleware.initLocals);
 keystone.pre('render', middleware.flashMessages);
+
+// Handle 404 errors
+keystone.set('404', (request, response) => {
+  response.notfound();
+});
+
+// Handle other errors
+keystone.set('500', (error, request, response) => {
+  let title;
+  let message;
+
+  if (error instanceof Error) {
+    message = error.message;
+    error = error.stack;
+  }
+
+  response.err(error, title, message);
+});
 
 // Import Route Controllers
 var routes = {
@@ -17,5 +37,8 @@ exports = module.exports = function(app) {
   // Views
   app.get('/', routes.views.index);
 
+  if (app.get('env') !== 'production') {
+    app.get('/_errors/500', () => { throw new Error('Test error'); });
+  }
 
 };
