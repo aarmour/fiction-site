2abb01cb9b46229884940ec50576458a9992d75a
Adam Armour <adam.armour@gmail.com>
2016-01-10 13:27:38 -0700

    chore(build): clean up gulpfile
    - Fix white space and formatting
    - Use ES2015 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,44 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var jshintReporter = require('jshint-stylish');
-var watch = require('gulp-watch');
-var shell = require('gulp-shell')
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+const jshintReporter = require('jshint-stylish');
+const watch = require('gulp-watch');
+const shell = require('gulp-shell')
+const sass = require('gulp-sass');
 
-var sass = require('gulp-sass');
-
-
-var paths = {
-  'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
-
-,
+const paths = {
+  'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json'],
   'style': {
     all: './public/styles/**/*.scss',
     output: './public/styles/'
   }
-
 };
 
 // gulp lint
-gulp.task('lint', function(){
+gulp.task('lint', () => {
   gulp.src(paths.src)
     .pipe(jshint())
     .pipe(jshint.reporter(jshintReporter));
 });
 
 // gulp watcher for lint
-gulp.task('watch:lint', function () {
+gulp.task('watch:lint', () => {
   gulp.watch(paths.src, ['lint']);
 });
 
-gulp.task('watch:sass', function () {
+gulp.task('watch:sass', () => {
   gulp.watch(paths.style.all, ['sass']);
 });
 
-gulp.task('sass', function(){
+gulp.task('sass', () => {
   gulp.src(paths.style.all)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(paths.style.output));
 });
 
 gulp.task('runKeystone', shell.task('node keystone.js'));
+
 gulp.task('watch', [
-
   'watch:sass',
-
   'watch:lint'
 ]);
 
